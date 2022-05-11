25b7e1f38e82ef29d8c100d3036555b5ae696a15
Cloudgen Wong <cloudgen.wong@gmail.com>
2022-05-11 11:14:48 +0000

    update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*!
  * xprezzo-stream-unpipe
- * Copyright(c) 2020 Ben Ajenoui <info@seohero.io>
+ * Copyright(c) 2022 Cloudgen Wong <cloudgen.wong@gmail.com>
  * MIT Licensed
  */
 
@@ -12,15 +12,15 @@
  */
 
 const hasPipeDataListeners = (stream) => {
-  if (typeof stream === 'object' && typeof stream.listeners === 'function') {
-    const listeners = stream.listeners('data')
-    for (let i = 0; i < listeners.length; i++) {
-      if (listeners[i].name === 'ondata') {
-        return true
-      }
+    if (typeof stream === 'object' && typeof stream.listeners === 'function') {
+        const listeners = stream.listeners('data')
+        for (let i = 0; i < listeners.length; i++) {
+            if (listeners[i].name === 'ondata') {
+                return true
+            }
+        }
     }
-  }
-  return false
+    return false
 }
 
 /**
@@ -30,25 +30,25 @@ const hasPipeDataListeners = (stream) => {
  * @public
  */
 module.exports = (stream) => {
-  if (!stream) {
-    throw new TypeError('argument stream is required')
-  }
-  if (typeof stream === 'object' && typeof stream.unpipe === 'function') {
-    // new-style
-    stream.unpipe()
-    return
-  }
-  // Node.js 0.8 hack
-  if (!hasPipeDataListeners(stream)) {
-    return
-  }
-  const listeners = stream.listeners('close')
-  for (let i = 0; i < listeners.length; i++) {
-    const listener = listeners[i]
-    if (listener.name !== 'cleanup' && listener.name !== 'onclose') {
-      continue
+    if (!stream) {
+        throw new TypeError('argument stream is required')
     }
-    // invoke the listener
-    listener.call(stream)
-  }
+    if (typeof stream === 'object' && typeof stream.unpipe === 'function') {
+        // new-style
+        stream.unpipe()
+        return
+    }
+    // Node.js 0.8 hack
+    if (!hasPipeDataListeners(stream)) {
+        return
+    }
+    const listeners = stream.listeners('close')
+    for (let i = 0; i < listeners.length; i++) {
+        const listener = listeners[i]
+        if (listener.name !== 'cleanup' && listener.name !== 'onclose') {
+            continue
+        }
+        // invoke the listener
+        listener.call(stream)
+    }
 }
